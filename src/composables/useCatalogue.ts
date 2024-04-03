e50fdee1ec7aabe1ba93552e968edf0280805bfa
gjssss <gaojinsong2010@gmail.com>
2024-04-03 17:21:32 +0800

    feat: delete category and rename file

--- src/composables/useCatalogue.ts
+++ src/composables/useCatalogue.ts
@@ -4,7 +4,7 @@ export async function useCatalogue() {
   const { data, refresh, close } = await useData<Category[]>('catalogue', [
     {
       id: nanoid(),
-      name: '未命名',
+      name: '未命名分类',
       files: [],
       isFold: false,
     },
@@ -13,10 +13,27 @@ export async function useCatalogue() {
     Str2Data: src => JSON.parse(src),
   })
 
+  function addCategory() {
+    const id = nanoid()
+    data.value.push({
+      id,
+      name: '未命名分类',
+      isFold: false,
+      files: [],
+    })
+    return id
+  }
+
+  function delCategory(idx: number) {
+    data.value.splice(idx, 1)
+  }
+
   return {
     data,
     refresh,
     close,
+    addCategory,
+    delCategory,
   }
 }
 
